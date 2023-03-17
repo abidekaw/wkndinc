10739d39492b836063d5ee05575ed83e42fc3019
abidekaw <abideka80@gmail.com>
2023-03-18 02:34:45 +0700

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,36 @@
-const swiper = new Swiper('.swiper', {
-   // Optional parameters
-   direction: 'horizontal',
-   loop: true,
-
-   // Navigation arrows
-   navigation: {
-      nextEl: '.prev',
-      prevEl: '.next',
-   },
-});
-
 fetch('https://challenge.fe.weekendinc.com/testimonial')
    .then((response) => response.json())
    .then((data) => {
       let result = '';
-      data.map((val) => {
-         result += `
-            <div class="bg-white text-black p-2-5 card-wrapper">
-               <div class="font-black text-3xl">${val.by}</div>
-               <p class="text-xs font-normal mt-5">${val.testimony}</p>
-            </div>
-         `;
-      });
-      document.querySelector('#slider').innerHTML = result;
+      if (data.length > 0) {
+         data.map((val) => {
+            result += `
+               <div class="bg-white text-black p-2-5 card-wrapper">
+                  <div class="font-black text-3xl">${val.by}</div>
+                  <p class="text-xs font-normal mt-5">${val.testimony}</p>
+               </div>
+            `;
+         });
+         document.querySelector('#slider').innerHTML = result;
+
+         // Inisialisasi Tiny Slider setelah HTML diatur secara dinamis
+         var slider = tns({
+            container: '#slider',
+            items: 2,
+            loop: false,
+            speed: 400,
+            controlsContainer: '.arrow-outer',
+            mouseDrag: true,
+            swipeAngel: false,
+         });
+      } else {
+         document.querySelector('#slider').innerHTML = 'No slides found.';
+      }
    })
-   .catch((err) => console.log(err));
+   .catch((err) => {
+      console.log(err);
+      document.querySelector('#slider').innerHTML = 'Error fetching data.';
+   });
 
 fetch('https://challenge.fe.weekendinc.com/help-tips')
    .then((response) => response.json())
