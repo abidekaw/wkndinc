440d8a42e02517dd2cb624bb2d7905839891f21a
abidekaw <abideka80@gmail.com>
2023-03-17 09:57:22 +0700

    third

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,50 @@
-const testimonial = 'https://challenge.fe.weekendinc.com/testimonial';
-const helptips = 'https://challenge.fe.weekendinc.com/help-tips';
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
 
-fetch(testimonial)
+   // Navigation arrows
+   navigation: {
+      nextEl: '.prev',
+      prevEl: '.next',
+   },
+});
+
+fetch('https://challenge.fe.weekendinc.com/testimonial')
    .then((response) => response.json())
    .then((data) => {
-      let testi = '';
+      let result = '';
       data.map((val) => {
-         testi += `
+         result += `
             <div class="bg-white text-black p-2-5 card-wrapper">
                <div class="font-black text-3xl">${val.by}</div>
                <p class="text-xs font-normal mt-5">${val.testimony}</p>
             </div>
          `;
       });
-      document.querySelector('#testi').innerHTML = testi;
+      document.querySelector('#slider').innerHTML = result;
    })
    .catch((err) => console.log(err));
 
-fetch(helptips)
+fetch('https://challenge.fe.weekendinc.com/help-tips')
    .then((response) => response.json())
    .then((data) => {
-      let help = '';
+      let result = '';
       data.map((val) => {
-         help += `
+         result += `
             <div class="w-77 relative">
                <img src="${val.image}" alt="${val.slug}" width="311" height="176">
                <div class="w-full absolute bottom-0 flex center evenly py-4 pl-6 bg-black" style="opacity: 0.5;">
                   <div class="text-white text-base w-54">
                      ${val.title}
                   </div>
-                  <a href="#" class="arrow-inner">
+                  <a href="#" class="arrow-inner" aria-label="read more about content">
                      <i class="fa-solid fa-arrow-right"></i>
                   </a>
                </div>
             </div>
          `;
       });
-      document.querySelector('#help-tips').innerHTML = help;
+      document.querySelector('#card').innerHTML = result;
    })
    .catch((err) => console.log(err));
